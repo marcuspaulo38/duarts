f92077a989ff53468c799660f6fdd677e9cd3e10
Marcus Paulo <marcusantoniopaulo@gmail.com>
2019-11-29 07:29:49 -0300

    Enviando o projeto para versionamento

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+define('LARAVEL_START', microtime(true));
+
+
+
+require __DIR__.'/../papup/vendor/autoload.php';
+
+
+
+$app = require_once __DIR__.'/../papup/bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
